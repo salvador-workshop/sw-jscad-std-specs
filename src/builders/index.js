2eb042bbef81cef2af9c53d5a8fee956e6ceeec9
RJ Salvador <salvador.workshop@gmail.com>
2025-05-30 20:49:52 -0400

    more bugfixing

--- src/builders/index.js
+++ src/builders/index.js
@@ -5,10 +5,16 @@ const init = ({ lib, swLib }) => {
         walls: require('./walls').init({ lib, swLib }),
     }
 
+    // gotta do this because of "builders" prop name conflict :(
+    const initBuilders = {
+        ...builders,
+        ...swLib.builders,
+    }
+
     // Dependent on libs, utils, and first builders
-    builders.entryways = require('./entryways').init({ lib, swLib: { ...swLib, builders } });
-    builders.roofs = require('./roofs').init({ lib, swLib: { ...swLib, builders } });
-    builders.buttress = require('./buttress').init({ lib, swLib: { ...swLib, builders } });
+    builders.entryways = require('./entryways').init({ lib, swLib: { ...swLib, builders: initBuilders } });
+    builders.roofs = require('./roofs').init({ lib, swLib: { ...swLib, builders: initBuilders } });
+    builders.buttress = require('./buttress').init({ lib, swLib: { ...swLib, builders: initBuilders } });
 
     return builders;
 }
