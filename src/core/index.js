e85a91dd1d46d58388497893f4013b50d3e5ab9a
RJ Salvador <salvador.workshop@gmail.com>
2025-06-10 18:39:38 -0400

    big file cleanup

--- src/core/index.js
+++ src/core/index.js
@@ -9,12 +9,9 @@ const init = ({ lib }) => {
     const core = {
         constants: require('./constants'),
         errors: require('./errors'),
-        position: require('./position').init({ lib }),
-        text: require('./text').init({ lib }),
     }
 
     core.internals = require('./internals').init({ lib, swLib: core });
-    core.parts = require('./parts').init({ lib, swLib: core });
 
     return core;
 }
--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
 const coreModule = require('./core');
-const utilsModule = require('./utils');
-const detailsModule = require('./details');
-const familiesModule = require('./families');
-const buildersModule = require('./builders');
 
 const init = ({ lib }) => {
     const swJscad = {
         core: coreModule.init({ lib }),
     }
-
-    swJscad.utils = utilsModule.init({ lib, swLib: swJscad });
-    swJscad.details = detailsModule.init({ lib, swLib: swJscad });
-    swJscad.families = familiesModule.init({ lib, swLib: swJscad });
-    swJscad.builders = buildersModule.init({ lib, swLib: swJscad });
-
     return swJscad;
 }
 
