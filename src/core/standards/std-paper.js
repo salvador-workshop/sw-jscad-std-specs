9b9516ffbf1b46efe8c3b7274fdb72ad427b15aa
RJ Salvador <salvador.workshop@gmail.com>
2025-07-04 13:48:40 -0400

    WIP - setting up paper standards

--- src/core/standards/std-paper.js
+++ src/core/standards/std-paper.js
@@ -1,11 +1,44 @@
 "use strict"
 
 const paperStd = ({ lib, swLib }) => {
+    const { constants, maths } = swLib.core
+
+    // STANDARD: [width, height]
+    const cards = {
+        BUSINESS_CARD: [maths.inchesToMm(3 / 8), maths.inchesToMm(3 / 8)],
+        CREDIT_CARD: [maths.inchesToMm(3 / 8), maths.inchesToMm(3 / 8)],
+        POSTCARD: [maths.inchesToMm(6), maths.inchesToMm(4)],
+    }
+
+    const metric = {
+        A4: [maths.inchesToMm(6), maths.inchesToMm(6)],
+    }
+
+    const imperial = {
+        LETTER: [maths.inchesToMm(6), maths.inchesToMm(6)],
+        TABLOID: [maths.inchesToMm(6), maths.inchesToMm(6)],
+        LEDGER: [maths.inchesToMm(6), maths.inchesToMm(6)],
+    }
+
+    const bookmarks = {
+        BOOKMARK: [maths.inchesToMm(6), maths.inchesToMm(4)],
+    }
+
+    const stock = {
+        BOARD: [maths.inchesToMm(6), maths.inchesToMm(4)],
+    }
+
+    const photos = {
+        EIGHT_BY_TEN: [maths.inchesToMm(8), maths.inchesToMm(10)],
+    }
+
     return {
-        A4: [999, 999],
-        LETTER: [999, 999],
-        TABLOID: [999, 999],
-        LEDGER: [999, 999],
+        cards,
+        metric,
+        imperial,
+        bookmarks,
+        stock,
+        photos
     }
 }
 
